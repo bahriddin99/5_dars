65c5f969f9aec6d56beb01d785f5d6f5fd86d6ea
Bahriddin <Bahriddin>
2023-10-24 23:05:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let age = prompt("Yoshingiz nichida?");
+let AGE = +age + 3
+let qaysiyosh = prompt("Qaysi yoshga kirdi?")
+let yilBorligi = +qaysiyosh - age
+console.log("2023" - age + "-" + "yilda tug'ulgan," + " " + " 3 yildan keyin " +  AGE + " ga to'ladi"+" " + yilBorligi+"yil bor xali");
